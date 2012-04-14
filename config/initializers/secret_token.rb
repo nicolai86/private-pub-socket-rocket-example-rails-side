0b03e61b56bcac713dfe5d5e5e95440ec3d9ac8a
Raphael Randschau <nicolai86@me.com>
2012-04-14 20:10:18 +0200

    rails new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PrivatePubSocketRocketExample::Application.config.secret_token = '19c0e87ec521e2cb3dfd57ebc76196f5c9a5e89c8d69b2e928c97d4ce885b3657d67fcc3bf4e78995c82a9287383d5c8e3a6d9188bc6788cbd4ebba66a90e987'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+PrivatePubSocketRocketExample::Application.config.session_store :cookie_store, key: '_private-pub-socket-rocket-example_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# PrivatePubSocketRocketExample::Application.config.session_store :active_record_store
